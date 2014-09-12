e3b1f50a7a8b4bd1f34ac0692f66e36c0db4bf47
Inok <club0174@gmail.com>
2014-09-12 04:28:37 +0400

    .net 4.5 project removed

--- VkNetAsync/Properties/AssemblyInfo.cs
+++ VkNetAsync/Properties/AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-#if !VKNET_PORTABLE
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -22,7 +21,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fc1b61d1-f470-4b42-9944-9b77cb86400c")]
-#endif
 
 // Version information for an assembly consists of the following four values:
 //
