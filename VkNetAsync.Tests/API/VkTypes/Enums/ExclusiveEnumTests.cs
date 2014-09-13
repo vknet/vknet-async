d750c1bba16fdeb23cce0fc43a24d46e8293dbc5
Inok <club0174@gmail.com>
2014-09-14 02:33:28 +0400

    exclusiveEnum class and tests

---
+++ VkNetAsync.Tests/API/VkTypes/Enums/ExclusiveEnumTests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using NUnit.Framework;
+using VkNetAsync.API.VkTypes.Enums;
+
+namespace VkNetAsync.Tests.API.VkTypes.Enums
+{
+	[TestFixture]
+	public class TestedExclusiveEnum : ExclusiveEnum<TestedExclusiveEnum>
+	{
+		#region RegistrationTests
+
+		[TestFixture]
+		public class ExclusiveEnumRegistrationTests : ExclusiveEnum<ExclusiveEnumRegistrationTests>
+		{
+			[Test]
+			public void Register_NotRegisteredMember_DoesNotThrow()
+			{
+				Assert.DoesNotThrow(() => RegisterPossibleValue(1, "member1"));
+			}
+
+			[Test]
+			public void Register_NotRegisteredMember_ReturnsRegisteredMember()
+			{
+				Assert.That(RegisterPossibleValue(2, "member2"), Is.SameAs(FromValue(2)));
+			}
+
+			[Test]
+			public void Register_MemberWithTheSameValueExists_ThrowsArgumentException()
+			{
+				RegisterPossibleValue(3, "member3");
+				Assert.Throws<ArgumentException>(() => RegisterPossibleValue(3, "memberName"));
+			}
+
+			[Test]
+			public void Register_MemberWithTheSameNameExists_ThrowsArgumentException()
+			{
+				RegisterPossibleValue(4, "member4");
+				Assert.Throws<ArgumentException>(() => RegisterPossibleValue(5, "member4"));
+			}
+
+		}
+
+		#endregion
+
+
+		private static readonly TestedExclusiveEnum MemberA =   RegisterPossibleValue(0, "memberA_name");
+		private static readonly TestedExclusiveEnum MemberB =   RegisterPossibleValue(10, "memberB_name");
+		private static readonly TestedExclusiveEnum MemberC =   RegisterPossibleValue(-5, "memberC_name");
+		private static readonly TestedExclusiveEnum MemberMax = RegisterPossibleValue(long.MaxValue, "memberMax_name");
+		private static readonly TestedExclusiveEnum MemberMin = RegisterPossibleValue(long.MinValue, "memberMin_name");
+
+		#region FromValue
+
+		[Test]
+		public void FromValue_RegisteredValue_ReturnsRegisteredMember()
+		{
+			Assert.That(FromValue(0), Is.SameAs(MemberA));
+			Assert.That(FromValue(10), Is.SameAs(MemberB));
+			Assert.That(FromValue(-5), Is.SameAs(MemberC));
+			Assert.That(FromValue(long.MaxValue), Is.SameAs(MemberMax));
+			Assert.That(FromValue(long.MinValue), Is.SameAs(MemberMin));
+		}
+
+		[Test]
+		public void FromValue_NotRegisteredValue_ThrowsArgumentOutOfRangeException()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => FromValue(-1));
+			Assert.Throws<ArgumentOutOfRangeException>(() => FromValue(-10));
+		}
+
+		#endregion
+
+
+		#region FromName
+
+		[Test]
+		public void FromName_RegisteredName_ReturnsRegisteredMember()
+		{
+			Assert.That(FromName("memberA_name"), Is.SameAs(MemberA));
+			Assert.That(FromName("memberB_name"), Is.SameAs(MemberB));
+			Assert.That(FromName("memberC_name"), Is.SameAs(MemberC));
+			Assert.That(FromName("memberMax_name"), Is.SameAs(MemberMax));
+			Assert.That(FromName("memberMin_name"), Is.SameAs(MemberMin));
+		}
+
+		[Test]
+		public void FromName_NotRegisteredName_ThrowsArgumentOutOfRangeException()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => FromName("notExisted"));
+			Assert.Throws<ArgumentOutOfRangeException>(() => FromName("notExistedToo"));
+		}
+
+		[Test]
+		public void FromName_NullName_ThrowsArgumentNullException()
+		{
+			// ReSharper disable once AssignNullToNotNullAttribute
+			Assert.Throws<ArgumentNullException>(() => FromName(null));
+		}
+
+		[Test]
+		public void FromName_EmptyName_ThrowsArgumentException()
+		{
+			Assert.Throws<ArgumentException>(() => FromName(string.Empty));
+		}
+
+		#endregion
+
+
+		#region Properties
+
+		[Test]
+		public void ValueProperty_ReturnsCorrectValue()
+		{
+			Assert.That(MemberA.Value, Is.EqualTo(0));
+			Assert.That(MemberB.Value, Is.EqualTo(10));
+			Assert.That(MemberC.Value, Is.EqualTo(-5));
+			Assert.That(MemberMax.Value, Is.EqualTo(long.MaxValue));
+			Assert.That(MemberMin.Value, Is.EqualTo(long.MinValue));
+		}
+
+		[Test]
+		public void NameProperty_ReturnsCorrectName()
+		{
+			Assert.That(MemberA.Name, Is.EqualTo("memberA_name"));
+			Assert.That(MemberB.Name, Is.EqualTo("memberB_name"));
+			Assert.That(MemberC.Name, Is.EqualTo("memberC_name"));
+			Assert.That(MemberMax.Name, Is.EqualTo("memberMax_name"));
+			Assert.That(MemberMin.Name, Is.EqualTo("memberMin_name"));
+		}
+
+		#endregion
+
+
+		#region Equality
+
+		[Test]
+		public void Equality_EqualMembers_ReturnsTrue()
+		{
+			// ReSharper disable once EqualExpressionComparison
+			Assert.True(MemberA == MemberA);
+			Assert.True(MemberMax.Equals(MemberMax));
+		}
+
+		[Test]
+		public void Equality_NotEqualMembers_ReturnsFalse()
+		{
+			// ReSharper disable once EqualExpressionComparison
+			Assert.False(MemberB == MemberC);
+			Assert.False(MemberMax.Equals(MemberMin));
+			Assert.True(MemberC != MemberA);
+		}
+
+		#endregion
+	}
+
+
+
+
+}
---
+++ VkNetAsync/API/VkTypes/Enums/ExclusiveEnum.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using VkNetAsync.Annotations;
+
+namespace VkNetAsync.API.VkTypes.Enums
+{
+	public class ExclusiveEnum<TExclusive> where TExclusive : ExclusiveEnum<TExclusive>, new()
+	{
+		private sealed class EnumMember
+		{
+			private readonly long _value;
+			public long Value
+			{
+				get { return _value; }
+			}
+
+			private readonly string _name;
+			public string Name
+			{
+				get { return _name; }
+			}
+
+			internal EnumMember(long value, string name)
+			{
+				_value = value;
+				_name = name;
+			}
+
+			public override string ToString()
+			{
+				return string.Format("{0} ({1})", Name, Value);
+			}
+		}
+		
+		#region Static members
+
+		// ReSharper disable once StaticFieldInGenericType
+		private static readonly IList<TExclusive> PossibleValues = new List<TExclusive>();
+
+		protected static TExclusive RegisterPossibleValue(long value, [NotNull] string name)
+		{
+			Contract.Requires<ArgumentNullException>(name != null);
+			Contract.Requires<ArgumentException>(name.Length > 0);
+
+			if (PossibleValues.Any(member => member.Value == value))
+				throw new ArgumentException(string.Format("Element of type {0} with value {1} is already registered ({2}).",
+														  typeof(TExclusive).FullName, value, FromValue(value)), 
+											"value");
+
+			if (PossibleValues.Any(member => member.Name == name))
+				throw new ArgumentException(string.Format("Element of type {0} with name {1} is not registered ({2}).",
+														  typeof(TExclusive).FullName, name, FromName(name)),
+											"name");
+
+			PossibleValues.Add(new TExclusive { _member = new EnumMember(value, name) });
+
+			return FromValue(value);
+		}
+
+		protected static TExclusive FromValue(long value)
+		{
+			var enumMember = PossibleValues.SingleOrDefault(member => member.Value == value);
+			if (enumMember == null)
+				throw new ArgumentOutOfRangeException("value", string.Format("Element of type {0} with value {1} is not registered.", typeof (TExclusive).FullName, value));
+			
+			return enumMember;
+		}
+
+		protected static TExclusive FromName([NotNull] string name)
+		{
+			Contract.Requires<ArgumentNullException>(name != null);
+			Contract.Requires<ArgumentException>(name.Length > 0);
+
+			var enumMember = PossibleValues.SingleOrDefault(member => member.Name == name);
+			if (enumMember == null)
+				throw new ArgumentOutOfRangeException("name", string.Format("Element of type {0} with name {1} is not registered.", typeof(TExclusive).FullName, name));
+
+			return enumMember;
+		}
+
+		#endregion
+
+		private EnumMember _member;
+
+		public long Value
+		{
+			get { return _member.Value; }
+		}
+
+		public string Name
+		{
+			get { return _member.Name; }
+		}
+
+		protected ExclusiveEnum()
+		{
+			_member = null;
+		}
+
+		public override string ToString()
+		{
+			return _member.ToString();
+		}
+
+		public static bool operator ==(ExclusiveEnum<TExclusive> left, ExclusiveEnum<TExclusive> right)
+		{
+			if (ReferenceEquals(right, left)) return true;
+			if (ReferenceEquals(null, left)) return false;
+			if (ReferenceEquals(null, right)) return false;
+
+			return left._member.Value == right._member.Value;
+		}
+
+		public static bool operator !=(ExclusiveEnum<TExclusive> left, ExclusiveEnum<TExclusive> right)
+		{
+			return !(left == right);
+		}
+
+		protected bool Equals(ExclusiveEnum<TExclusive> other)
+		{
+			return this == other;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
+			return Equals((ExclusiveEnum<TExclusive>)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return _member.Value.GetHashCode();
+		}
+
+	}
+}
